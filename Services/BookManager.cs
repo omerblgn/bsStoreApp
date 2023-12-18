6964fdc6f2981b9205e4ea625a6e25c974d14655
omerblgn <omerbilgen01@gmail.com>
2023-12-18 14:37:00 +0300

    nlog has been implemented.

--- Services/BookManager.cs
+++ Services/BookManager.cs
@@ -7,17 +7,16 @@ namespace Services
     public class BookManager : IBookService
     {
         private readonly IRepositoryManager _manager;
+        private readonly ILoggerService _logger;
 
-        public BookManager(IRepositoryManager manager)
+        public BookManager(IRepositoryManager manager, ILoggerService logger)
         {
             _manager = manager;
+            _logger = logger;
         }
 
         public Book CreateOneBook(Book book)
         {
-            if (book is null)
-                throw new ArgumentNullException(nameof(book));
-
             _manager.Book.CreateOneBook(book);
             _manager.Save();
             return book;
@@ -27,7 +26,11 @@ namespace Services
         {
             var entity = _manager.Book.GetOneBookById(id, trackChanges);
             if (entity is null)
-                throw new Exception($"Book with id: {id} could not found.");
+            {
+                string message = $"The book with id: {id} could not found.";
+                _logger.LogInfo(message);
+                throw new Exception(message);
+            }
 
             _manager.Book.DeleteOneBook(entity);
             _manager.Save();
@@ -47,7 +50,11 @@ namespace Services
         {
             var entity = _manager.Book.GetOneBookById(id, trackChanges);
             if (entity is null)
-                throw new Exception($"Book with id: {id} could not found.");
+            {
+                string message = $"The book with id: {id} could not found.";
+                _logger.LogInfo(message);
+                throw new Exception(message);
+            }
 
             entity.Title = book.Title;
             entity.Price = book.Price;
